430e44d60a40365f668dd3e55748a60bc7fb6192
Irina_Sharafutdinova <irinsharafutdinov@yandex.ru>
2022-07-13 21:14:07 +0300

    Задача №27 к уроку №4 решена!

---
+++ C+HomeWork/Lesson_4_Task_27/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+* 452 -> 11
+* 82 -> 10
+* 9012 -> 12 */
+void DigitSum (int number)
+{
+    int current = 0, result = 0;
+    while (number > 0)
+    {
+        current = number%10; 
+        number = number/10;
+        result = result + current;
+    }
+    Console.WriteLine("Сумма цифр заданного числа равна: " + result);
+}
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+DigitSum(N);
