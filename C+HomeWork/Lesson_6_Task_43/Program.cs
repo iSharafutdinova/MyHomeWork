34a4836697149550dbd7ab581ecdb7960df53889
Irina_Sharafutdinova <irinsharafutdinov@yandex.ru>
2022-07-19 20:52:41 +0300

    Задача №43 к уроку №6 решена!

--- C+HomeWork/Lesson_6_Task_43/Program.cs
+++ C+HomeWork/Lesson_6_Task_43/Program.cs
@@ -1,26 +1,26 @@
 ﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
 y = k1 * x + b1, y = k2 * x + b2; 
 значения b1, k1, b2 и k2 задаются пользователем.
-* b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5) */
+* b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5) 
 
-void CreateArray(double[] array)
+y = k1 * x + b1
+y = k2 * x + b2
+k1 * x + b1 = k2 * x + b2
+x * (k1 - k2) = b2 - b1
+x = (b2 - b1) / (k1 - k2)
+*/
+
+double FindX(double b1, double k1, double b2, double k2)
 {
-    for (int index = 0; index < array.Length; index++) 
-    {
-        Console.Write(array[index] + " ");
-    }
-    Console.WriteLine();        
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
 }
 
-double[] Find(double x1, double y1, double x2, double y2)
+double FindY(double b1, double k1, double b2, double k2)
 {
-    double[] Point = {0, 0};
-    if (y1 == y2)
-    {
-        Point[0] = (x2 - x1) / (y2 - y1);
-        Point[1] = y1 * Point[0] + x1;
-    }
-    return Point;
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    return y;
 }
 
 Console.Write("Введите значение b1: ");
@@ -32,5 +32,9 @@ double b2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите значение k2: ");
 double k2 = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Координаты точки пересечения двух прямых: ");
-CreateArray(Math.Round(Find(b1, k1, b2, k2),2));
+if (k1 == k2) Console.WriteLine("Пересечения не найдено. Прямые параллельны!");
+else {
+    double x = FindX(b1, k1, b2, k2);
+    double y = FindY(b1, k1, b2, k2);
+    Console.WriteLine("Координаты точки на плоскости: (" + x + "; " + y + ")");
+}
