37ac61442baa6f8f6d11e1fb0884fd4f98e63b16
Irina_Sharafutdinova <irinsharafutdinov@yandex.ru>
2022-07-19 20:11:22 +0300

    Задача №41 к уроку №6 решена!

---
+++ C+HomeWork/Lesson_6_Task_41/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+* 0, 7, 8, -2, -2 -> 2
+* 1, -7, 567, 89, 223-> 3 */
+
+void CreateArray(int[] array, int length)
+{
+    int index = 0;
+    while (index < length) 
+    {
+        array[index] = new Random().Next(-10, 10);
+        index++;
+    }
+}
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.Write(array[index] + " ");
+        index++;
+    }
+}
+void CountPositive(int[] array)
+{
+    int result = 0, index = 0;
+    while (index < array.Length)
+    {
+        if (array[index] > 0) result++;
+        index++;
+    }
+    Console.WriteLine(", количество положительных чисел равно: " + result);
+}
+Console.Write("Введите длину массива М: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+CreateArray(array, number);
+PrintArray(array);
+CountPositive(array);
---
+++ C+HomeWork/Lesson_6_Task_43/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+* b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5) */
+
+void CreateArray(double[] array)
+{
+    for (int index = 0; index < array.Length; index++) 
+    {
+        Console.Write(array[index] + " ");
+    }
+    Console.WriteLine();        
+}
+
+double[] Find(double x1, double y1, double x2, double y2)
+{
+    double[] Point = {0, 0};
+    if (y1 == y2)
+    {
+        Point[0] = (x2 - x1) / (y2 - y1);
+        Point[1] = y1 * Point[0] + x1;
+    }
+    return Point;
+}
+
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Координаты точки пересечения двух прямых: ");
+CreateArray(Math.Round(Find(b1, k1, b2, k2),2));
