c92b8cbc09e8bcc3a208ae07765640a9007a45dd
Irina_Sharafutdinova <irinsharafutdinov@yandex.ru>
2022-07-21 19:50:02 +0300

    Задача №54 к уроку № 8 (с большим трудом) решена!

---
+++ C+HomeWork/Lesson_8_Task_54/Program.cs
@@ -0,0 +1,59 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+В итоге получается вот такой массив:
+1 2 4 7
+2 3 5 9
+2 4 4 8 */
+
+double[,] Create2DArray(int rows, int columns, int min, int max)
+{
+    double[,] array = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(min, max + 1);
+    return array;
+}
+
+void Print2DArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+}
+
+double[,] SortingMinToMaxInRows(double[,] array)
+{
+    double temp;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int m = j + 1; m < array.GetLength(1); m++)
+            {
+                if (array[i, j] > array[i, m])
+                {
+                    temp = array[i, j];
+                    array[i, j] = array[i, m];
+                    array[i, m] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] myArray = Create2DArray(m, n, 1, 9);
+Print2DArray(myArray);
+Console.WriteLine();
+Print2DArray(SortingMinToMaxInRows(myArray));
