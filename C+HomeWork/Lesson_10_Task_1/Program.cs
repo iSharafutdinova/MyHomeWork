aea3ff868c43452d13b5724bdc855acbc7f27aae
Irina_Sharafutdinova <irinsharafutdinov@yandex.ru>
2022-07-29 20:45:27 +0300

    Задачи №1, №2 к уроку №10 решены

---
+++ C+HomeWork/Lesson_10_Task_1/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Задача 1: Задайте массив строк. Напишите программу, считает кол-во слов в массиве, начинающихся на гласную букву.
+Пример: { "qwe", "wer", "ert", "rty", "tyu"} -> 1 */
+
+string[] CreateStringArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите слово №" + (i + 1) + ": ");
+        string newArray = Convert.ToString(Console.ReadLine());
+        array[i] = newArray;
+    }
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(array[i] + "  ");
+    }
+    Console.WriteLine();
+}
+
+int CountWords(string[] array)
+{
+    int length = array.Length;
+    string vowels = "aeiouy";
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i][0] == vowels[0] || array[i][0] == vowels[1] || array[i][0] == vowels[2] || array[i][0] == vowels[3] || array[i][0] == vowels[4] || array[i][0] == vowels[5])
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Задайте размер массива (сколько слов будет всего): ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string[] myArray = CreateStringArray(N);
+
+PrintArray(myArray);
+
+Console.WriteLine("Количество слов в массиве, начинающихся на глаcную букву: " + CountWords(myArray));
---
+++ C+HomeWork/Lesson_10_Task_2/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Задача 2: Задайте массив строк. Напишите программу, которая генерирует новый массив, объединяя элементы исходного массива попарно.
+Пример: { "qwe", "wer", "ert", "rty", "tyu", "yui"} -> { "qwewer", "ertrty", "tyuyui"} */
+
+string[] CreateStringArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите слово для массива " + (i + 1) + ": ");
+        string newArray = Convert.ToString(Console.ReadLine());
+        array[i] = newArray;
+    }
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(array[i] + "  ");
+    }
+    Console.WriteLine();
+}
+
+string[] СoncatenationArray (string[] arrayFirst, string[] arraySecond) {
+    int lengthFirst = arrayFirst.Length;
+    string[] newArray = new string[lengthFirst];
+    for (int i = 0; i < lengthFirst; i++)
+    {
+        newArray[i] = arrayFirst[i] + arraySecond[i];
+    }
+    return newArray;
+}
+
+Console.Write("Введите номер числа для двух массивов: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string[] myFirstArray = CreateStringArray(N);
+
+string[] mySecondArray = CreateStringArray(N);
+
+PrintArray(myFirstArray);
+PrintArray(mySecondArray);
+
+string[] resultArray = СoncatenationArray(myFirstArray, mySecondArray);
+
+PrintArray(resultArray);
