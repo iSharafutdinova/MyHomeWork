bdba7fe8c9940f8e8bbe82a706071d18592d019f
Irina_Sharafutdinova <irinsharafutdinov@yandex.ru>
2022-07-29 19:13:32 +0300

    Задача №64 к уроку №9 решена!

---
+++ C+HomeWork/Lesson_9_Task_64/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
+N = 4532 -> 4 */
+
+int CountDigitsInNumber(int number)
+{
+    if (number / 10 != 0) return 1 + CountDigitsInNumber(number / 10);
+    else return 1;
+}
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Количество цифр в заданном числе: " + CountDigitsInNumber(N));
